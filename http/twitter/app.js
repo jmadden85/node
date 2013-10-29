7088c5cae63f1781c230f165c82d2c092e8a49aa
Joe Madden <Joe@beardbro.com>
2013-10-29 01:12:25 -0400

    more twitter setup

--- http/twitter/app.js
+++ http/twitter/app.js
@@ -1,22 +1,59 @@
 var http = require('http');
+var qs = require('querystring');
 
-http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('Hello World');
-}).listen(3000);
+var key = 'iiB7qKuOGCm0gAT9QomIg';
+var secret = 'L8qq9PZyRg6ieKGEKhZolGC0vJWLw8iEJ88DRdyOg';
+var token = key + ':' + secret;
+var base64Token = new Buffer(token).toString('base64');
 
+var headers = {
+    'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
+    'Authorization' : 'Basic ' + base64Token
+};
+
+//Get token
+var tweetToken = http.request({
+    host : 'api.twitter.com',
+    method : 'POST',
+    headers : headers
+}, function (res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+      res.on('data', function (chunk) {
+        console.log('BODY: ' + chunk);
+      });
+});
+
+tweetToken.on('error', function(e) {
+  console.log('problem with request: ' + e.message);
+});
+
+tweetToken.write('grant_type=client_credentials');
+tweetToken.end();
+
+var search = process.argv.slice(2).join(' ').trim();
+
+if ( !search.length ) {
+    return console.log('\n Usage: node tweets <search term>\n');
+}
+
+console.log('\n searching for: \033[96m' + search + '\033[39m\n');
 http.request({
-    host : '127.0.0.1',
-    port : 3000,
-    url : '/',
-    method : 'GET'
+    host : 'api.twitter.com/1.1',
+    path : '/search/tweets.json?' + qs.stringify({ q : search })
 }, function (res) {
     var body = '';
     res.setEncoding('utf8');
     res.on('data', function (chunk) {
         body += chunk;
     });
-    res.on('end', function() {
-        console.log('\n We got: \033[96m' + body + '\033[39m\n');
+    res.on('end', function () {
+        var obj = JSON.parse(body);
+        obj.results.forEach(function (tweet) {
+            console.log(' \033[90m' + tweet.text + '\033[39m');
+            console.log(' \033[94m' + tweet.from_user + '\033[39m');
+            console.log('--');
+        });
     });
 }).end();
